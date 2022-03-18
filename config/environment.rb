182dd2793baa2afab26ee7b10658f02b4f4ad556
davepcage <71347825+davepcage@users.noreply.github.com>
2022-03-18 10:50:47 -0600

    BLOG: Create a signup email.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.perform_deliveries
+# ActionMailer::Base.smtp_settings = {
+# 	addres: "smtp.gmail.com",
+# 	port: 465,
+# 	domain: "gmail.com",
+# 	user_name: ENV['GMAIL_USERNAME'],
+# 	password: ENV['GMAIL_PASSWORD'],
+# 	authentication: 'plain',
+# 	ssl: true,
+# 	tsl: true,
+# 	enable_starttls_auto: true
+# }
